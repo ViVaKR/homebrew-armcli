dbafea3181c55b1afb849c0189204965c05ca976
Kim Bum Jun <admin@vivabm.com>
2026-09-04 18:44:45 +0900

    chore: bump armcli to v0.3.0
    - url/sha256/version 을 v0.3.0 릴리스 자산 기준으로 갱신
    - 자산: armcli-v0.3.0-osx-arm64.tar.gz
    - sha256: 1bb98f174b65fa523fdd5043087d5ebc4d04a94363480264907c606b556e7975

--- armcli.rb
+++ armcli.rb
@@ -1,9 +1,9 @@
 class Armcli < Formula
   desc "ARM64 Assembly Template Generator CLI for Students"
   homepage "https://github.com/ViVaKR/armcli"
-  url "https://github.com/ViVaKR/armcli/releases/download/v0.2.0/armcli-v0.2.0-osx-arm64.tar.gz"
-  sha256 "0062007658ba9062052e2ff2ffa82f15d292918b70dba4ea8c6340e741c6e451"
-  version "0.2.0"
+  url "https://github.com/ViVaKR/armcli/releases/download/v0.3.0/armcli-v0.3.0-osx-arm64.tar.gz"
+  sha256 "1bb98f174b65fa523fdd5043087d5ebc4d04a94363480264907c606b556e7975"
+  version "0.3.0"
 
   def install
     bin.install "armcli"
